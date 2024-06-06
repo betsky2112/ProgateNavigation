0c5fcf206172ca0d41f9466fab2b005c49a3593b
betsky2112 <roberthussebastians@gmail.com>
2024-06-06 11:24:19 +0700

    Develop Home Screen

--- App.js
+++ App.js
@@ -1,64 +1,23 @@
-import React from 'react'
-import {View, Text, Button, Image, StyleSheet} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-import {createDrawerNavigator} from '@react-navigation/drawer'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import React from 'react'
+import Home from './screens/home'
 
-const HomeScreen = ({navigation}) => {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.marginBottom20}>Home Screen</Text>
-			<Button
-				style={styles.button}
-				title="Pergi ke Progate"
-				onPress={() => {
-					navigation.navigate('Progate')
-				}}
-			/>
-		</View>
-	)
-}
-
-const ProgateScreen = ({navigation}) => {
-	return (
-		<View style={styles.container}>
-			<Text>Welcome to Progate</Text>
-			<Text style={styles.marginBottom20}>Ayo belajar!</Text>
-			<Button
-				title="Kembali"
-				onPress={() => navigation.goBack()}
-			/>
-		</View>
-	)
-}
-
-const Drawer = createDrawerNavigator()
+const Stack = createNativeStackNavigator()
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Drawer.Navigator initialRouteName="Home">
-				<Drawer.Screen
-					component={HomeScreen}
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen
 					name="Home"
+					component={Home}
 				/>
-				<Drawer.Screen
-					component={ProgateScreen}
-					name="Progate"
-				/>
-			</Drawer.Navigator>
+				<Stack.Screen />
+				<Stack.Screen />
+			</Stack.Navigator>
 		</NavigationContainer>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	marginBottom20: {
-		marginBottom: 20,
-	},
-})
 
 export default App
---
+++ screens/home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Button, StyleSheet, Text, View} from 'react-native'
+
+const home = ({navigation}) => {
+	return (
+		<View style={styles.container}>
+			<Button
+				title="Pergi ke Progate"
+				onPress={() => navigation.navigate('Progate')}
+			/>
+			<Text style={styles.marginVertical20}>atau</Text>
+			<Button
+				title="Hubungi Kami"
+				onPress={() => navigation.navigate('Contact')}
+			/>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	marginVertical20: {
+		marginVertical: 20,
+	},
+})
+
+export default home
